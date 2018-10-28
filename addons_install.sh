d99dc1c1cc4664465c886ffea9195a9072719b6b
Luis J <luis.gomez@kairosds.com>
2018-10-28 12:16:18 +0100

    Lets add the new files

---
+++ addons_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script para la instalación de los módulos esenciales
+# para una funcionamiento molón del Odoo.
+# Desarrollado por etham 
+
+# Algo de Florituras. Le damos color
+
+while read LINE
+    do echo "Hoy tenemos un $addon"
+done < addons_list.txt
